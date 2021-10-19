b9bf13ef8c1e2647853f460cbf9d61eaf2bb1f96
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-19 17:20:38 +0200

    add files with UserMeneger class to project

---
+++ UserMeneger.h
@@ -0,0 +1,21 @@
+#ifndef USERMENEGER_H
+#define USERMENEGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+#include "User.h"
+#include "SupportingMethods.h"
+
+using namespace std;
+
+class UserMeneger
+{
+public:
+
+private:
+
+};
+
+#endif // USERMENEGER_H
