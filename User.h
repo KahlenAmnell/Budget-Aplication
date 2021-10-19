f4268d27c1682ea4d25737c49c514c0f5402197f
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-19 14:24:39 +0200

    add setters and getters to class User

--- User.h
+++ User.h
@@ -8,9 +8,24 @@ using namespace std;
 class User
 {
 public:
+    void setId(int newId);
+    void setName(string newName);
+    void setLastName(string newLastName);
+    void setLogin(string newLogin);
+    void setPassword(string newPassword);
+
+    int getId();
+    string getName();
+    string getLastName();
+    string getLogin();
+    string getPassword();
 
 private:
-
+    int id;
+    string name;
+    string lastName;
+    string login;
+    string password;
 };
 
 #endif // USER_H
