488f893a77539f4f1a3f1eebb7574de6d2e3aaaf
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-19 14:46:21 +0200

    add methods to class SupportingMethods: convertStringToInteger(), enterSign(), enterInteger(), loadLine()

--- SupportingMethods.h
+++ SupportingMethods.h
@@ -1,9 +1,19 @@
 #ifndef SUPPORTINGMETHODS_H
 #define SUPPORTINGMETHODS_H
 
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
 class SupportingMethods
 {
 public:
+    static int convertStringToInteger(string number);
+    static char enterSign();
+    static int enterInteger();
+    static string loadLine();
 
 private:
 
