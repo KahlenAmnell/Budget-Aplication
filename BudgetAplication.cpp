489a4c5799881c25485f28f57b991b47722f2886
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-19 13:25:42 +0200

    add BudgetAplication files to project

---
+++ BudgetAplication.cpp
@@ -0,0 +1,1 @@
+#include "BudgetAplication.h"
---
+++ BudgetAplication.h
@@ -0,0 +1,16 @@
+#ifndef BUDGETAPLICATION_H
+#define BUDGETAPLICATION_H
+
+#include <iostream>
+
+using namespace std;
+
+class BudgetAplication
+{
+public:
+
+private:
+
+};
+
+#endif // BUDGETAPLICATION_H
