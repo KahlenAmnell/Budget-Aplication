f72b8e12eaebad3d779c38f12027a79924fe4330
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-20 19:39:32 +0200

    add class Finances files

---
+++ Finances.cpp
@@ -0,0 +1,1 @@
+#include "Finances.h"
---
+++ Finances.h
@@ -0,0 +1,16 @@
+#ifndef FINANCES_H
+#define FINANCES_H
+
+#include <iostream>
+
+using namespace std;
+
+class Finances
+{
+public:
+
+private:
+
+};
+
+#endif // FINANCES_H
