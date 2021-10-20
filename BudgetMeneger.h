1047a7e6ad788fef8fcbcaf102435aae35f9e3be
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-20 19:32:42 +0200

    add BudgetMeneger files to project

---
+++ BudgetMeneger.h
@@ -0,0 +1,15 @@
+#ifndef BUDGETMENEGER_H
+#define BUDGETMENEGER_H
+
+#include <iostream>
+#include "SupportingMethods.h"
+
+class BudgetMeneger
+{
+public:
+
+private:
+
+};
+
+#endif // BUDGETMENEGER_H
