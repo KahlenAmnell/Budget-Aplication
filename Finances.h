d389748acdd0794cc2db10d4ae1b26fa98b8043c
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-20 19:46:03 +0200

    add setters and getters to class Finances

--- Finances.h
+++ Finances.h
@@ -8,9 +8,18 @@ using namespace std;
 class Finances
 {
 public:
+    void setUserId(int newUserId);
+    void setDate(int newDate);
+    void setAmount(int newAmount);
+
+    int getUserId();
+    int getDate();
+    int getAmount();
 
 private:
-
+    int userId;
+    int date;
+    int amount;
 };
 
 #endif // FINANCES_H
