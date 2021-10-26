c786de1152322a9641e42c61eb79099e0f8f7fae
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-10-26 15:59:21 +0200

    Ad files with class FileWithUsers

---
+++ FileWithUsers.h
@@ -0,0 +1,16 @@
+#ifndef FILEWITHUSERS_H
+#define FILEWITHUSERS_H
+
+#include "User.h"
+#include "Markup.h"
+
+using namespace std;
+
+class FileWithUsers
+{
+public:
+
+private:
+
+};
+#endif // FILEWITHUSERS_H
