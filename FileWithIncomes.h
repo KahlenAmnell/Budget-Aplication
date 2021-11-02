4155b2931a83272232c33f76fb906a45f6496cdb
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-11-02 11:48:51 +0100

    add files with class FileWithIncomes to project

---
+++ FileWithIncomes.h
@@ -0,0 +1,20 @@
+#ifndef FILEWITHINCOMES_H
+#define FILEWITHINCOMES_H
+
+#include "Markup.h"
+#include "Incomes.h"
+#include "XMLFile.h"
+#include "SupportingMethods.h"
+#include <vector>
+
+using namespace std;
+
+class FileWithIncomes :public XmlFile
+{
+public:
+
+private:
+
+};
+
+#endif // FILEWITHINCOMES_H
