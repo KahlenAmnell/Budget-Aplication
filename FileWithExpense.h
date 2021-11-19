a94b3e9522eda865f28ce1a502f58fdea5ba8b05
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-11-19 16:22:17 +0100

    add files with class FileWithExpense to project

---
+++ FileWithExpense.h
@@ -0,0 +1,21 @@
+#ifndef FILEWITHEXPENSE_H
+#define FILEWITHEXPENSE_H
+
+#include "Markup.h"
+#include "Expense.h"
+#include "XMLFile.h"
+#include "SupportingMethods.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class FileWithExpense :public XmlFile
+{
+public:
+
+private:
+
+};
+
+#endif // FILEWITHEXPENSE_H
