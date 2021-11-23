9872c2f3a222711793e9951411f3031d20419a65
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2021-11-23 16:25:50 +0100

    add files with class DateMeneger to project

---
+++ DateMeneger.h
@@ -0,0 +1,16 @@
+#ifndef DATEMENEGER_H
+#define DATEMENEGER_H
+
+#include <ctime>
+#include "SupportingMethods.h"
+
+using namespace std;
+
+class DateMeneger
+{
+public:
+
+private:
+
+};
+#endif // DATEMENEGER_H
